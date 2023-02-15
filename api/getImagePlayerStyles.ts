a9a9390174200f920a0c13dd8ebfaeaf1095f15c
Gil Barbara <gilbarbara@gmail.com>
2023-02-15 03:24:46 -0300

    Update getImagePlayerStyles bgColor
    - add quantity query param

--- api/getImagePlayerStyles.ts
+++ api/getImagePlayerStyles.ts
@@ -1,10 +1,10 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { fade, textColor } from 'colorizr';
+import { darken, fade, lighten, luminance, textColor } from 'colorizr';
 import { fileTypeFromBuffer } from 'file-type';
 import getImageColors from 'get-image-colors';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const url = request.query.url as string;
+  const { quantity, url } = request.query as Record<string, string>;
 
   if (!url) {
     response.status(400).send('No url provided');
@@ -13,8 +13,22 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const fetchUrl = await fetch(url);
   const arrayBuffer = await fetchUrl.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
-
   const fileType = await fileTypeFromBuffer(buffer);
+
+  if (quantity) {
+    const colors = await getImageColors(buffer, {
+      count: parseInt(quantity, 10),
+      type: fileType?.mime,
+    });
+
+    if (!colors?.length) {
+      response.status(500).send('No colors found');
+    }
+
+    response.send(colors.map(color => color.hex()));
+
+    return;
+  }
 
   const colors = await getImageColors(buffer, {
     count: 2,
@@ -27,11 +41,16 @@ export default async (request: VercelRequest, response: VercelResponse) => {
 
   const altColor = colors[1].hex();
   const baseColor = colors[0].hex();
-  const color = textColor(baseColor);
+  const colorLuminance = luminance(baseColor);
+
+  const maxValue = Math.min(Math.max(Math.round(colorLuminance * 100), 10), 70);
+
+  const bgColor = colorLuminance <= 0.02 ? lighten(baseColor, 10) : darken(baseColor, maxValue);
+  const color = textColor(bgColor);
 
   response.send({
     altColor,
-    bgColor: baseColor,
+    bgColor,
     color,
     sliderColor: color,
     sliderHandleColor: color,
