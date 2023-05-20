5a3995d392ed19a1ffe04475cea103aa23333957
Gil Barbara <gilbarbara@gmail.com>
2023-05-20 20:43:35 -0300

    Add svgLogosGitHubAuthentication

---
+++ api/svgLogosGitHubAuthentication.ts
@@ -0,0 +1,57 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+const admins = ['gilbarbara'];
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { code } = request.query as Record<string, string>;
+
+  if (request.method !== 'GET') {
+    return response.status(405).end();
+  }
+
+  if (!code) {
+    return response.status(400).send('No code provided');
+  }
+
+  const {
+    SVGLOGOS_GITHUB_CLIENT_ID = '',
+    SVGLOGOS_GITHUB_CLIENT_SECRET = '',
+    SVGLOGOS_GITHUB_REDIRECT_URI = '',
+  } = process.env;
+
+  const formData = new FormData();
+
+  formData.append('client_id', SVGLOGOS_GITHUB_CLIENT_ID);
+  formData.append('client_secret', SVGLOGOS_GITHUB_CLIENT_SECRET);
+  formData.append('redirect_uri', SVGLOGOS_GITHUB_REDIRECT_URI);
+  formData.append('code', code);
+
+  const authentication = await fetch('https://github.com/login/oauth/access_token', {
+    body: formData,
+    headers: {
+      Accept: 'application/json',
+    },
+    method: 'POST',
+  }).then(data => data.json());
+
+  if (authentication.error) {
+    return response.status(401).send(authentication.error_description);
+  }
+
+  const user = await fetch('https://api.github.com/user', {
+    headers: {
+      Authorization: `token ${authentication.access_token}`,
+    },
+  }).then(data => data.json());
+
+  if (!admins.includes(user.login)) {
+    return response.status(401).end();
+  }
+
+  return response.send({
+    accessToken: authentication.access_token,
+    avatar: user.avatar_url,
+    login: user.login,
+    profile: user.html_url,
+  });
+};
