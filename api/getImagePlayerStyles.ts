4ccfddc130f5f3b640e94ea9e6cd2eb91a0bb7b3
Gil Barbara <gilbarbara@gmail.com>
2024-11-28 15:32:09 -0300

    Update getImagePlayerStyles color algorithm

--- api/getImagePlayerStyles.ts
+++ api/getImagePlayerStyles.ts
@@ -1,10 +1,28 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { darken, fade, lighten, luminance, textColor } from 'colorizr';
+import { darken, lighten, luminance, opacify, textColor } from 'colorizr';
 import { fileTypeFromBuffer } from 'file-type';
 import getImageColors from 'get-image-colors';
 
+function generateColor(input: string, max: number) {
+  const colorLuminance = Math.round(luminance(input) * 100);
+  let maxValue = Math.min(Math.max(colorLuminance, 10), max);
+  let bgColor = darken(input, maxValue);
+
+  if (colorLuminance <= 0.02) {
+    bgColor = lighten(input, 10);
+  } else {
+    while (luminance(bgColor) * 100 < 3) {
+      bgColor = darken(input, maxValue);
+      maxValue -= 1;
+    }
+  }
+
+  return [bgColor, textColor(bgColor)];
+}
+
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const { quantity, url } = request.query as Record<string, string>;
+  const { max = '70', quantity, url } = request.query as Record<string, string>;
+  const maxAmount = parseInt(max, 10);
 
   if (request.method === 'OPTIONS') {
     return response.status(200).end();
@@ -45,23 +63,18 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     response.status(500).send('No colors found');
   }
 
-  const altColor = colors[1].hex();
-  const baseColor = colors[0].hex();
-  const colorLuminance = luminance(baseColor);
-
-  const maxValue = Math.min(Math.max(Math.round(colorLuminance * 100), 10), 70);
-
-  const bgColor = colorLuminance <= 0.02 ? lighten(baseColor, 10) : darken(baseColor, maxValue);
-  const color = textColor(bgColor);
+  const [bgColor, color] = generateColor(colors[0].hex(), maxAmount);
+  const [altBgColor, altColor] = generateColor(colors[1].hex(), maxAmount);
 
   return response.send({
+    altBgColor,
     altColor,
     bgColor,
     color,
     sliderColor: color,
     sliderHandleColor: color,
-    sliderTrackColor: fade(color, 70),
-    trackArtistColor: fade(color, 30),
+    sliderTrackColor: opacify(color, 0.3),
+    trackArtistColor: opacify(color, 0.7),
     trackNameColor: color,
   });
 };
